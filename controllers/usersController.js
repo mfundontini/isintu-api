c96ffcc378412cbc138590b2401d973cf0329c57
MfundoNtini <mfundontini@gmail.com>
2020-07-10 00:24:21 +0200

    FEATURE: Added users app controller.

---
+++ controllers/usersController.js
@@ -0,0 +1,34 @@
+exports.listAllUsers = (request, response) => {
+  response.status(501).json({
+    status: "Fail",
+    message: "Not implemented"
+  });
+};
+
+exports.getUser = (request, response) => {
+  response.status(501).json({
+    status: "Fail",
+    message: "Not implemented"
+  });
+};
+
+exports.updateUser = (request, response) => {
+  response.status(501).json({
+    status: "Fail",
+    message: "Not implemented"
+  });
+};
+
+exports.createUser = (request, response) => {
+  response.status(501).json({
+    status: "Fail",
+    message: "Not implemented"
+  });
+};
+
+exports.deleteUser = (request, response) => {
+  response.status(501).json({
+    status: "Fail",
+    message: "Not implemented"
+  });
+};
