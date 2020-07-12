5743549c7c601634d711db3651794fe85b20ff33
MfundoNtini <mfundontini@gmail.com>
2020-07-12 14:41:07 +0200

    FEATURE: Added advanced filtering.

--- controllers/proverbsController.js
+++ controllers/proverbsController.js
@@ -1,5 +1,8 @@
 const Proverb = require("./../schemas/proverbs");
 
+// Constants
+const FILTERING_EXCLUSIONS = ["limit", "offset", "sort", "order"];
+
 // Middlewares
 exports.validateId = (request, response, next, value) => {
     if(value.length !== 24 ) return response.status(401).json({
@@ -31,8 +34,22 @@ exports.findProverbOrExit = (request, response, next) => {
 exports.listAllProverbs = async (request, response) => {
 
   try {
-    const proverbs = await Proverb.find();
-    
+    // Declare query with all results
+    let query = Proverb.find();
+
+    // Filtering
+    const queryString = request.query;
+    if(Object.keys(queryString).length !== 0) {
+      console.log(queryString);
+      let filters = {...queryString};
+
+      FILTERING_EXCLUSIONS.forEach(el => delete filters[el]);
+
+      query.find(filters);  
+    }
+
+    // Resolve query
+    const proverbs = await query;
     // Use status codes to communicate actions than an umbrella error
     if(proverbs.length > 0) {
        return response.status(200).json({
