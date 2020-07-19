50f58e2e3cf2ee6d8c5c223b41e75742e68805e4
MfundoNtini <mfundontini@gmail.com>
2020-07-20 00:56:27 +0200

    FEATURE: Added generic Error class.

---
+++ utils/apiError.js
@@ -0,0 +1,12 @@
+class APIError extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.status = `${status}`.startsWith("4") ? "Fail" : "Error";
+        this.statusCode = statusCode;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+
+module.exports = APIError;
